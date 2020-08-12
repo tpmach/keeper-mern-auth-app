45b8119240a770f175eb51439b76aeb42cec56a2
tpmach <thi.p.mach@gmail.com>
2020-08-12 03:27:07 -0700

    organized the folders

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ mongoose.connect(
 
 //set up for run on Heroku
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"))
+  app.use(express.static("client/build"))
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   })
 }
 
